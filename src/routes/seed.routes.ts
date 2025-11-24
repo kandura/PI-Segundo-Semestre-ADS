192440a12b2bc2cf93369daeb6e4b174de2bded9
kandura <81191275+kandura@users.noreply.github.com>
2025-11-23 21:48:18 -0300

    Update no seed.routes.ts

--- src/routes/seed.routes.ts
+++ src/routes/seed.routes.ts
@@ -11,16 +11,16 @@ router.post("/seed/mesas", async (req, res) => {
   try {
     await prisma.mesa.createMany({
   data: [
-    { codigo: "M01", nome: "Mesa 1" },
-    { codigo: "M02", nome: "Mesa 2" },
-    { codigo: "M03", nome: "Mesa 3" },
-    { codigo: "M04", nome: "Mesa 4" },
-    { codigo: "M05", nome: "Mesa 5" },
-    { codigo: "M06", nome: "Mesa 6" },
-    { codigo: "M07", nome: "Mesa 7" },
-    { codigo: "M08", nome: "Mesa 8" },
-    { codigo: "M09", nome: "Mesa 9" },
-    { codigo: "M10", nome: "Mesa 10" },
+    { codigo: "M01",},
+    { codigo: "M02",},
+    { codigo: "M03",},
+    { codigo: "M04",},
+    { codigo: "M05",},
+    { codigo: "M06",},
+    { codigo: "M07"},
+    { codigo: "M08"},
+    { codigo: "M09"},
+    { codigo: "M10"" },
   ],
 });
 

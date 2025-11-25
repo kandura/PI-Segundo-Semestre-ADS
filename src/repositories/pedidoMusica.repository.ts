73b51f07dac4b9aa184f393750c6028300b82f51
kandura <81191275+kandura@users.noreply.github.com>
2025-11-25 20:10:52 -0300

    ajuste no pedidoMusica.repository.ts

--- src/repositories/pedidoMusica.repository.ts
+++ src/repositories/pedidoMusica.repository.ts
@@ -1,34 +1,55 @@
 import prisma from "../database/prismaClient.js";
+import { Prisma, PedidoStatus } from "@prisma/client";
 
 export const PedidoMusicaRepository = {
+  // criar pedido
+  create(data: {
+    clienteId: number;
+    musicId: number;
+    mesaId: number;
+    status?: string;
+  }) {
+    const { status, ...rest } = data;
 
-    //criar pedido
-    create(data: {clienteId : number; musicId: number; mesaId: number })
-    {
-        return prisma.pedidoMusica.create({
-            data });
-    },
+    return prisma.pedidoMusica.create({
+      data: {
+        ...rest,
+        // se vier status, converte para enum do Prisma
+        ...(status ? { status: status as PedidoStatus } : {}),
+      },
+    });
+  },
 
-    //listar todos os pedidos (com filtro por status opicional)
-    findAll(status?: string, mesaId?: number){
-        return prisma.pedidoMusica.findMany({
-            where: {
-                ...(status ? {status } : {}),
-                ...(mesaId ? {mesaId} : {})
-            },
-            include: {
-                music: true,
-                cliente: true,
-            },
-            orderBy: { createdAt: "asc"},
-        });
-    },
+  // listar todos os pedidos (com filtro por status opcional)
+  findAll(status?: string, mesaId?: number) {
+    const where: Prisma.PedidoMusicaWhereInput = {};
 
-    //att status
-    updateStatus(id: number, status: string){
-        return prisma.pedidoMusica.update({
-            where: {id},
-            data: {status},
-        });
-    },
+    if (status) {
+      // converte de string para enum PedidoStatus
+      where.status = status as PedidoStatus;
+    }
+
+    if (typeof mesaId === "number") {
+      where.mesaId = mesaId;
+    }
+
+    return prisma.pedidoMusica.findMany({
+      where,
+      include: {
+        music: true,
+        cliente: true,
+      },
+      orderBy: { createdAt: "asc" },
+    });
+  },
+
+  // atualizar status
+  updateStatus(id: number, status: string) {
+    return prisma.pedidoMusica.update({
+      where: { id },
+      data: {
+        status: status as PedidoStatus,
+      },
+    });
+  },
 };

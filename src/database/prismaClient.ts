3c5bd76d22e622d104e0f7647e29f9d3551f39a6
Caique Dorival dos Santos <caiquedorivalsantos@gmail.com>
2025-11-29 16:37:32 -0300

    fix: remove dist antigo do projeto

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,28 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
 
-const { Pool } = pkg;
-
-// Render passa DATABASE_URL automaticamente
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL não encontrada no ambiente!");
-}
-
-// Criar pool do PostgreSQL
-const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false } // Render exige SSL
-});
-
-// Criar adapter Prisma 7
-const adapter = new PrismaPg(pool);
-
-// Criar PrismaClient usando o adapter
-const prisma = new PrismaClient({
-  adapter,
-});
+const prisma = new PrismaClient();
 
 export default prisma;
